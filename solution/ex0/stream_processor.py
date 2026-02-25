5d5cddd67430170af50d5f09f21d458c1067826e
Zack <girardzack04@example.com>
2026-02-25 12:33:44 +0100

    advancing

--- solution/ex0/stream_processor.py
+++ solution/ex0/stream_processor.py
@@ -8,22 +8,59 @@ class DataProcessor(ABC):
         self.data = data
     
     @abstractmethod
-    def process(self, data: Any):
+    def process(self, data: Any) -> str:
         pass
     
     @abstractmethod
-    def validate(self, data: Any):
+    def validate(self, data: Any) -> bool:
         pass
 
-    def format_output(self, result : str):
+    def format_output(self, result : str) -> str:
         """has a default behavior, needs to be overridden"""
-        print("Processed: example")
-        pass
-class NumericProcessor(DataProcessor):
-    pass
+        return f"Processed: {result}"
 
+class NumericProcessor(DataProcessor):
+
+    def process(self, data : Any):
+        return f"Processing data: {data}"
+    
+    def validate(self, data : Any):
+        if isinstance(data, list):
+            count = 0
+            total_sum = 0
+            try:
+                for e in data:
+                    total_sum += int(e)
+                    count += 1
+                return True
+            except Exception:
+                return False
+        else:
+            return False
+    def format_output(self, result : str) -> str:
+        return f"output {result}"
+
+                
 class LogProcessor(DataProcessor):
-    pass
+   pass
+
+
+def test():
+    print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===\n")
+    numbers = ["1", "2", "3", "4", "5"]
+
+    print("Initializing numeric processor...")
+    data = NumericProcessor(numbers)
+    data.process(numbers)
+    res = data.validate(numbers)
+    if res == True:
+        print("Validation: Numeric data verified")
+    print(data.format_output())
+
+
+
+if __name__ == '__main__':
+    test()
 
 
 
