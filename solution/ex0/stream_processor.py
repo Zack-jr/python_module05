f4ddf4ec7406b5749d935203eca9354ecab1d86c
Zakaria Labib-Girard <girard.zack04@gmail.com>
2026-02-26 14:34:41 +0100

    ex0

--- solution/ex0/stream_processor.py
+++ solution/ex0/stream_processor.py
@@ -22,7 +22,6 @@ class DataProcessor(ABC):
 class NumericProcessor(DataProcessor):
 
     def process(self, data : Any)-> str:
-        print(f"Processing data: {data}")
         count = len(data)
         numbers = (float(n) for n in data)
         total_sum = sum(numbers)
@@ -31,16 +30,11 @@ class NumericProcessor(DataProcessor):
     
     def validate(self, data : Any):
         if isinstance(data, List):
-            count = 0
-            total_sum = 0
-            try:
-                for e in data:
-                    total_sum += int(e)
-                    count += 1
-                print("Validation: Numeric data verified")
-                return True
-            except Exception:
-                return False
+            for n in data:
+                if isinstance(n, (float, int)) == False:
+                    return False
+            print("Validation: Numeric data verified")
+            return True
         else:
             return False
 
@@ -52,7 +46,6 @@ class TextProcessor(DataProcessor):
    
     def process(self, data) -> str:
         try:
-            print(f'Processing data: "{data}"')
             length = len(data)
             word_count = len(data.split(" "))
             return f"Processed text: {length} characters, {word_count} words"
@@ -71,38 +64,65 @@ class TextProcessor(DataProcessor):
 class   LogProcessor(DataProcessor):
 
     def process(self, data) -> str:
-        print(f"processing data: {data}")
-
+        prefix_suffix = []
+        prefix_suffix = data.split(":", 1)
+        if prefix_suffix[0] == "ERROR":
+            return f"[ALERT] ERROR level detected: {prefix_suffix[1]}"
+        if prefix_suffix[0] == "INFO":
+            return f"[INFO] INFO level detected: {prefix_suffix[1]}"
+    
     def validate(self, data: Any) -> bool:
-        return True
+        logs = ["ERROR", "INFO"]
+        for log in logs:
+            if log in data:
+                return True
+        return False
+
     def format_output(self, result: str) -> str:
         return f"Output: {result}"
     
 
 def test():
     print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===\n")
-    numbers = ["1", "2", "3", "4", "5"]
+    numbers = [1, 2, 3, 4, 5]
 
     print("Initializing Numeric Processor...")
     data = NumericProcessor(numbers)
+    print(f'Processing data: "{numbers}"')
     result = data.process(numbers)
     data.validate(numbers)
     print(data.format_output(result))
 
-
     print("\nInitializing Text Processor...")
     text = "This subject is not so good"
-    data2 = TextProcessor(text)
-    result2 = data2.process(text)
-    data2.validate(text)
-    print(data2.format_output(result2))
-
+    data = TextProcessor(text)
+    print(f'Processing data: "{text}"')
+    result = data.process(text)
+    data.validate(text)
+    print(data.format_output(result))
 
     print("\nInitializing Log Processor...")
-    data3 = LogProcessor()
-    result3 = data3.process(error)
-    data3.validate(error)
-    print(data3.format_output(result3))
+    error = "ERROR: Connection timeout"
+    data = LogProcessor(error)
+    print(f'Processing data: "{error}"')
+    result = data.process(error)
+    data.validate(error)
+    print(data.format_output(result))
+
+    print("\n=== Polymorphic Processing demo ===")
+    num = [1, 2, 3]
+    data = NumericProcessor(num)
+    print(f"Result 1: {data.process(num)}")
+
+    str = "hello world!"
+    data = TextProcessor(str)
+    print(f"Result 2: {data.process(str)}")
+
+    error = "INFO: System ready"
+    data = LogProcessor(error)
+    print(f"Result 3: {data.process(error)}")
+
+    print("\nFoundation systems online. Nexus ready for advanced streams.")
 
 
 if __name__ == '__main__':
