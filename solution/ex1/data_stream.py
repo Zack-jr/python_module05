b4c9d964514b98e9e6aadfcc529fd9fb5f351463
Zakaria Labib-Girard <girard.zack04@gmail.com>
2026-02-27 15:25:10 +0100

    save

--- solution/ex1/data_stream.py
+++ solution/ex1/data_stream.py
@@ -4,23 +4,27 @@ from typing import Any, List, Optional, Dict, Union
 
 class DataStream(ABC):
 
-    def __init__(self, stream_id):
+    def __init__(self, stream_id: str):
         self.data = None
         self.stream_id = stream_id
 
     @abstractmethod
     def process_batch(self, data_batch: List[Any]) -> str:
-        pass
+        return data_batch
 
     def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
-        pass
+        return data_batch
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
         pass
 
 
 class TransactionStream(DataStream):
-     
+    
+    def __init__(self, stream_id: str):
+        super().__init__(stream_id)
+        self.stream_id = stream_id
+
     def process_batch(self, data_batch: List[Any]) -> str:
         pass
 
@@ -32,6 +36,10 @@ class TransactionStream(DataStream):
 
 
 class EventStream(DataStream):
+
+    def __init__(self, stream_id: str):
+        super().__init__(stream_id)
+        self.stream_id = stream_id
 
     def process_batch(self, data_batch: List[Any]) -> str:
         pass
@@ -45,14 +53,36 @@ class EventStream(DataStream):
 
 class SensorStream(DataStream):
     
+    def __init__(self, stream_id: str):
+        super().__init__(stream_id)
+        self.processed_count = 0
+        self.total_temp = 0
+
+
     def process_batch(self, data_batch: List[Any]) -> str:
-        pass
+        print(f"Stream ID : {self.stream_id}, type: Environmental Data")
+        try:
+            for data in data_batch:
+                prefix, suffix = data.split(":", 1)
+                if prefix == "temp":
+                    self.total_temp += float(suffix)
+                self.processed_count += 1
+
+        except ValueError:
+            return ("Error!")
+        return f"Processing sensor batch: [{data_batch[0]}, {data_batch[1]}, {data_batch[2]}]"
+    
 
     def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
-        pass
+        print()
     
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
-        pass
+        if self.processed_count > 0:
+            average = self.total_temp / self.processed_count
+        else:
+            average = self.total_temp
+        sensor_dict = {"count": self.processed_count, "avg tmp": average}
+        return sensor_dict
 
 
 
@@ -71,10 +101,11 @@ class StreamProcessor():
 def main():
     print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===\n")
     print("Initializing Sensor Stream...")
-    batch = [22.5, 65, 1013]
+    batch = ['temp:22.5', 'humidity:65', 'pressure:1013', 'temp:12.2']
+
     stream1 = SensorStream("SENSOR_001")
-    stream1.filter_data(batch, "Environmental Data")
-    stream1.process_batch()
+    print(stream1.process_batch(batch))
+    #stream1.filter_data(batch, "Environmental Data")
 
 
     print("Initializing Transaction Stream...")
@@ -82,6 +113,8 @@ def main():
 
     print("Initializing Event Stream")
 
+if __name__ == '__main__':
+    main()
 
 
 
